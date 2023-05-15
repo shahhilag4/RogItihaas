f48801ef8a59fd9a3d121e37fd9afe5bda2c8644
shahhilag4 <shahhilag16@gmail.com>
2023-05-15 23:22:38 +0530

    Basic Modal

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -17,3 +17,23 @@ switchMode.addEventListener("change", function () {
     document.body.classList.remove("dark");
   }
 });
+
+const form = document.getElementById("myForm");
+
+// add event listener to form submit event
+form.addEventListener('submit', function(event) {
+  // prevent form from submitting
+  event.preventDefault();
+
+  // show the modal
+  const modal = document.getElementById('myModal');
+  // get the submit button inside the modal
+  const submitBtn = document.getElementById('submitBtn');
+
+  // add event listener to the submit button
+  submitBtn.addEventListener('click', function() {
+    // submit the form
+    form.submit();
+  });
+
+});
