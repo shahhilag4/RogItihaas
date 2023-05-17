c3942db8dde97853fb4e6da43e5c37b3e495ad20
shahhilag4 <shahhilag16@gmail.com>
2023-05-18 01:36:33 +0530

    Bussiness plan added

--- static/js/healthcard.js
+++ static/js/healthcard.js
@@ -1,5 +1,5 @@
 /* Health card content fetching */
 
-var healthcard = document.getElementById("back-img");
-var hcname = document.getElementById("name");
-hcname.innerHTML = healthcard.src.split("/")[healthcard.src.split("/").length -1].split(".")[0];
+// var healthcard = document.getElementById("back-img");
+// var hcname = document.getElementById("name");
+// hcname.innerHTML = healthcard.src.split("/")[healthcard.src.split("/").length -1].split(".")[0];
